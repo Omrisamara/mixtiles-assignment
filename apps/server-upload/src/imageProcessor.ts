8ff5ba261efa160875074a11439dec65157af8c6
Omri samara <omrisamara@gmail.com>
2025-04-14 11:33:38 +0300

    remove console times

--- apps/server-upload/src/imageProcessor.ts
+++ apps/server-upload/src/imageProcessor.ts
@@ -39,48 +39,31 @@ export class ImageProcessor {
 
   
   async processImages(files: Express.Multer.File[]): Promise<ClusterData[]> {
-    console.time('getDescriptions');
     const imagesVisionDescriptions = await this.googleVisionApi.getDescriptions(files);
-    console.timeEnd('getDescriptions');
     
-    console.time('getEmbddings');
     const descriptionsEmbddings = await this.openaiApi.getEmbddings(imagesVisionDescriptions);
-    console.timeEnd('getEmbddings');
 
     const [imagesTakenTime, imagesTakenLocation] = await this.exiftool.getImagesTakenTimeAndLocation(files);
 
-    console.time('cluster descriptions');
     const clusteredDescriptions = await this.clusterApi.cluster(
       descriptionsEmbddings,
       imagesTakenTime,
       imagesTakenLocation
     );
-    console.timeEnd('cluster descriptions');
 
-    console.time('createClusterNames');
     const clusterLabelsMap = await this.openaiApi.createClusterNames(
       clusteredDescriptions,
       imagesVisionDescriptions
     );
-    console.timeEnd('createClusterNames');
-
-    console.log('all clusters');
-    console.log(Array.from(clusterLabelsMap.keys()).length)
-
-    // // Print all cluster descriptions
-    // console.log('Cluster descriptions:');
-    // for (const [clusterId, clusterInfo] of clusterLabelsMap.entries()) {
-    //   console.log(`Cluster ${clusterId}: ${clusterInfo} - ${clusterInfo.description}`);
-    // }
 
     const relevantClusters = await this.openaiApi.getRelevantClusters(clusterLabelsMap);
-    console.log('relevantClusters', relevantClusters);
+    
+    console.log("Number of clusters: ", Array.from(clusterLabelsMap.keys()).length)
+    console.log("Number of relevant clusters: ", relevantClusters.length)
 
     const relevantFiles = filterFiles(files, clusteredDescriptions, relevantClusters);
     
-    console.time('saveImages');
     const fileNameToUrlMap = await this.googleCloudStorage.saveImages(relevantFiles);
-    console.timeEnd('saveImages');
 
     // Filter cluster mappings to only include files from relevant clusters
     const relevantClusterMappings = clusteredDescriptions.fileClusterMapping.filter(
